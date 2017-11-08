27b4443b708c064af76a5a0225da8bd2b9c3157b
MokumJ <jesse.vanderheijden@gmail.com>
2017-11-08 17:47:09 +0100

    show page event fixed, still have to figure out categories

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -44,7 +44,7 @@ class EventsController < ApplicationController
      .permit(
        :name, :description, :location,  :price,  :capacity,
          :includes_food,  :includes_drinks, :starts_at,  :ends_at,
-           :active,  :created_at,  :updated_at,  :user_id, category_ids: []
+           :active,  :created_at,  :updated_at,  :user_id, :categories
      )
  end
 end
