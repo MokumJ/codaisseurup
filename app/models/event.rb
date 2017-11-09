e0a084f9e052bf03cf3bdb6928f72d970820a366
MokumJ <jesse.vanderheijden@gmail.com>
2017-11-09 14:51:51 +0100

    add pictures

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,14 +1,15 @@
 class Event < ApplicationRecord
   belongs_to :user
- has_and_belongs_to_many :catogeries
+  has_and_belongs_to_many :categories
+  has_many :photos
+
+#<-------------validations----------------------------------->
+
   validates :name, presence: true
-
- validates :location, presence: true
-
- validates :capacity, presence: true, length: { maximum: 50000 }
-
- validates :description, presence: true, length: { maximum: 500 }
-
+  validates :location, presence: true
+  validates :capacity, presence: true, length: { maximum: 50000 }
+  validates :description, presence: true, length: { maximum: 500 }
+#<--------------methods-------------------------------------->
  def self.order_by_price
     order :price
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
+Photo.destroy_all
+Profile.destroy_all
 Category.destroy_all
 User.destroy_all
+Event.destroy_all
+
+
 
 party = Category.create!(name: "Party")
 buissiness_meeting = Category.create!(name: "Buissiness meeting")
@@ -13,16 +18,20 @@ miriam = User.create!({email: "miriam@codaisseurbnb.com", password: "abcd1234"})
 wouter = User.create!({email: "wouter@codaisseurbnb.com", password: "abcd1234"})
 matt = User.create!({email: "matt@codaisseurbnb.com", password: "abcd1234"})
 
-Event.create!({
+event_1 = Event.create!({
   name: "Buitenspelen", description: "Gezellig buitenspelen bij jou in de buurt!", location: "westerpark",  price: 0,  capacity: 100,
   includes_food: false,  includes_drinks: false, starts_at: Time.at(1364046539),  ends_at: Time.at(1364046539),
-  active: false, user: jesse})
-  Event.create!({
+  active: true, user: jesse})
+  event_2 = Event.create!({
   name: "Disco", description: "Disco feest, voetjes van de vloer", location: "Zaal 100",  price: 10,  capacity: 500,
   includes_food: false ,  includes_drinks: true , starts_at: Time.at(1364046539),  ends_at: Time.at(1364046539) ,
-  active: false,   user: miriam  })
+ active: true,   user: miriam  } )
 
-Event.create!({
+event_3 = Event.create!({
   name: "Verjaardagsfeestje", description: "Verjaardag van oom piet", location: "Lutjebroek",  price: 0,  capacity: 20,
   includes_food: true ,  includes_drinks: true, starts_at: Time.at(1364046539),  ends_at: Time.at(1364046539) ,
-  active: false,   user: wouter,})
+  active: false,   user: wouter })
+
+photo1 = Photo.create!(remote_image_url: "http://res.cloudinary.com/djhc9jhiq/image/upload/v1510230476/buitenspelen_fnae8l.jpg", event: event_1)
+photo2 = Photo.create!(remote_image_url: "http://res.cloudinary.com/djhc9jhiq/image/upload/v1510230742/piet_is_jarig_qocz9w.png", event: event_3)
+photo3 = Photo.create!(remote_image_url: "http://res.cloudinary.com/djhc9jhiq/image/upload/v1510230749/verjaardag_tgiknd.jpg", event: event_3)
